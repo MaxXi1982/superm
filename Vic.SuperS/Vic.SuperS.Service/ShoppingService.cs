e21a68ef5973c5804626d183d88ad9c5f7c0a89c
Max Xi <59275452@qq.com>
2018-08-05 04:30:14 +0800

    Vic.SuperS

---
+++ Vic.SuperS/Vic.SuperS.Service/ShoppingService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vic.SuperS.Data.Model;
+using Vic.SuperS.Data.Repository;
+
+namespace Vic.SuperS.Service
+{
+    public class ShoppingService
+    {
+        private readonly IShoppingCartRepository _shoppingCartRepository = new MockShoppingCartRepository();
+
+        private readonly IProductRepository _productRepository = new MockProductRepository();
+
+        public List<Product> GetAllProducts()
+        {
+            return _productRepository.GetAll();
+        }
+
+        public ShoppingCart CreateShoppingCart()
+        {
+            return _shoppingCartRepository.Create();
+        }
+        //ICIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII  check out
+        public Product BuyProduct(int shoppingcartId, int productId, int count = 1)
+
+        public Receipt Checkout(int shoppingcartId)
+        {
+            var shoppingCart = _shoppingCartRepository.GetById(shoppingcartId);
+
+            var receipt = _shoppingCartRepository.Create();
+
+            receipt.ShoppingCardId = shoppingCartId;
+            receipt.ShoppingItems = shoppingCart.ShoppingItems;
+            receipt.TotalPrice = shoppingCart.ShoppingItems.Sum(i => i.TotalPrice);
+
+            return receipt;
+        }
+        //  meixiewan   
+        public List<ShoppingItem> GetSHoppingC
+        public void RemoveProduct(int shoppingCartId, int productId, int count = 1)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException("Cannot buy product less then zero");
+            }
+
+            var cart = _shoppingCartRepository.GetById(shoppingCartId);
+            var product = _productRepository.GetById(productId);
+
+            var shoppingItem = cart.ShoppingItems.FirstOrDefault(i => i.ProductId == productId);
+
+            if (shoppingItem != null)
+            {
+                shoppingItem.Count += count;
+            }
+            else
+            {
+                var newShoppingItem = new ShoppingItem
+                {
+                    ProductId = productId,
+                    Price = product.Price,
+                    Count = count
+                };
+
+                cart.ShoppingItems.Add(newShoppingItem);
+            }
+
+            return product;
+        }
+
+        public object Checkout(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveProduct(int shoppingCartId, int productId, int count = 1)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException("Cannot remove product less then zero");
+            }
+
+            var cart = _shoppingCartRepository.GetById(shoppingCartId);
+            var product = _productRepository.GetById(productId);
+
+            var shoppingItem = cart.ShoppingItems.FirstOrDefault(i => i.ProductId == productId);
+
+            if (shoppingItem != null)
+            {
+                shoppingItem.Count -= count;
+
+                if (shoppingItem.Count < 0)
+                {
+                    shoppingItem.Count = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Vic.SuperS/Vic.SuperS.UI.ConsoleApp/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vic.SuperS.Service;
+
+namespace Vic.SuperS.UI.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingService shoppingService = new ShoppingService();
+
+            var allProducts = shoppingService.GetAllProducts();
+            allProducts.ForEach(i => Console.WriteLine(i));
+
+            var cart = shoppingService.CreateShoppingCart();
+
+            string input = "";
+            bool canExit = false;
+
+            do
+            {
+                Console.WriteLine("please input product id or command...");
+
+                input = Console.ReadLine().ToLower();
+
+                switch (input)
+                {
+                    case "exit":
+                    case "quit":
+                        canExit = true;
+                        break;
+                    case "checkout":
+                        // call check out logic
+
+                        var receipt = shoppingService.Checkout(cart.Id);
+                        Console.WriteLine(receipt);
+
+                        canExit = true;
+                        break;
+                    case "list":
+                        var list = shoppingService GetShoppingCartItems(cart.Id);
+                        list.ForEach(in => Console.WriteLine(i));
+                        break;
+                    default:
+                        break;
+                }
+
+                // remove 1234
+                if (input startsWith(removeCmd))
+                {
+                    string id = input.Substring(removeCmd.Lenght);
+                    int prodid = ParseProductId(id);
+
+                    if (prodid > 0)
+
+                }
+
+                int productId = 0;
+
+                if (int.TryParse(input, out productId))
+                {
+                    var buy = shoppingService.BuyProduct(cart.Id, productId);
+                    Console.WriteLine(buy.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("please input right product id");
+                }
+
+            } while (!canExit);
+
+            cart.ShoppingItems.ForEach(i => Console.WriteLine($"ProductId={i.ProductId}, Count={i.Count}, TotalPrice={i.TotalPrice}"));
+
+            Console.WriteLine("thank you, bye");
+            Console.ReadLine();
+        }
+    }
+}
